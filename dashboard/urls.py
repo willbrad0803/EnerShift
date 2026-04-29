d391f87fdb890d66abaaba72e289acf84a7e5e43
willbrad0803 <140106827+willbrad0803@users.noreply.github.com>
2026-04-29 11:42:04 +0100

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,15 +7,13 @@ urlpatterns = [
         template_name='registration/login.html',
         redirect_authenticated_user=True
     ), name='login'),
-
     path('register/', views.register, name='register'),
-    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
-    path('site/<int:site_id>/upload/', views.upload_consumption, name='upload_consumption'),
 
     path('', views.dashboard_home, name='dashboard_home'),
     path('overview/', views.all_sites_overview, name='all_sites_overview'),
     path('add-site/', views.add_site, name='add_site'),
     path('site/<int:site_id>/', views.site_detail, name='site_detail'),
+    path('site/<int:site_id>/upload/', views.upload_consumption, name='upload_consumption'),
     path('site/<int:site_id>/delete/', views.delete_site, name='delete_site'),
     path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
 ]
