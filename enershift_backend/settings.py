afa52f3d3b892a66f655d26bb89f51fbf5ffb4d0
willbrad0803 <140106827+willbrad0803@users.noreply.github.com>
2026-04-30 14:07:23 +0100

    Update settings.py

--- enershift_backend/settings.py
+++ enershift_backend/settings.py
@@ -61,7 +61,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'enershift_backend.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'), conn_max_age=600)
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # Fallback for local
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 STATIC_URL = '/static/'
