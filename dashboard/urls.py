17b67f76208d40bd0a2b4b782f09e914780664db
willbrad0803 <140106827+willbrad0803@users.noreply.github.com>
2026-05-19 13:58:53 +0100

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,10 +3,13 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    # Dashboard Routes
+    # Dashboard
     path('', views.dashboard_home, name='dashboard_home'),
-    
-    # Auth Routes
+    path('add-site/', views.add_site, name='add_site'),
+    path('site/<int:site_id>/', views.site_detail, name='site_detail'),
+    path('delete/<int:site_id>/', views.delete_site, name='delete_site'),
+
+    # Auth
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', views.custom_logout, name='logout'),
     path('register/', views.register, name='register'),
