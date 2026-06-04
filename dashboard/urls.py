2ca3fd9f327d5fc305169a027bc9cbaf5237a429
willbrad0803 <140106827+willbrad0803@users.noreply.github.com>
2026-06-04 12:19:43 +0100

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,4 +8,14 @@ urlpatterns = [
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', views.custom_logout, name='logout'),
+
+        # Password Reset
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
