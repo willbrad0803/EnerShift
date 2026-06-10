df6338dbf48726a010fa992b29abe12071c2f7e4
willbrad0803 <140106827+willbrad0803@users.noreply.github.com>
2026-06-10 22:35:58 +0100

    Update urls.py

--- enershift_backend/urls.py
+++ enershift_backend/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # Allauth (this is the main auth system)
+    # Allauth Authentication
     path('accounts/', include('allauth.account.urls')),
     
     # Dashboard
     path('dashboard/', include('dashboard.urls')),
     
-    # Temporary root redirect to login for now
-    path('', lambda r: redirect('account_login'), name='home'),
+    # Root -> Login
+    path('', lambda request: redirect('account_login'), name='home'),
 ]
