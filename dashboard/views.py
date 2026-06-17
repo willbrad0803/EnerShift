92d86cc8bdeaec07eb6668eb987a49c233b664e7
willbrad0803 <140106827+willbrad0803@users.noreply.github.com>
2026-06-17 20:38:43 +0100

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,12 +4,10 @@ from django.contrib import messages
 from .models import Site
 from .forms import SiteForm
 
-
 @login_required
 def dashboard_home(request):
     sites = Site.objects.filter(user=request.user)
     return render(request, 'dashboard/home.html', {'sites': sites})
-
 
 @login_required
 def add_site(request):
@@ -19,15 +17,13 @@ def add_site(request):
             site = form.save(commit=False)
             site.user = request.user
             site.save()
-            messages.success(request, f"Site '{site.name}' added successfully!")
+            messages.success(request, f"Site '{site.name}' added!")
             return redirect('dashboard_home')
     else:
         form = SiteForm()
     return render(request, 'dashboard/add_site.html', {'form': form})
 
-
 def custom_logout(request):
     from django.contrib.auth import logout
     logout(request)
-    messages.success(request, "You have been logged out.")
     return redirect('account_login')
