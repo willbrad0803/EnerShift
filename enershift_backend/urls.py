8498f4bda83a92f5d2c370b0dd886b177441a654
willbrad0803 <140106827+willbrad0803@users.noreply.github.com>
2026-06-19 23:01:46 +0100

    Update urls.py

--- enershift_backend/urls.py
+++ enershift_backend/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('accounts/', include('allauth.account.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('', TemplateView.as_view(template_name='public/index.html'), name='home'),  # Real homepage
+    path('about/', TemplateView.as_view(template_name='public/about.html'), name='about'),
+    path('pricing/', TemplateView.as_view(template_name='public/pricing.html'), name='pricing'),
+    path('contact/', TemplateView.as_view(template_name='public/contact.html'), name='contact'),
+    path('demo/', TemplateView.as_view(template_name='public/demo.html'), name='demo'),
+    
 ]
