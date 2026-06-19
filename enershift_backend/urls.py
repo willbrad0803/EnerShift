7f51107b46cd3f14b06cb47b59690a9c2c0ac7eb
willbrad0803 <140106827+willbrad0803@users.noreply.github.com>
2026-06-19 23:00:56 +0100

    Update urls.py

--- enershift_backend/urls.py
+++ enershift_backend/urls.py
@@ -1,10 +1,12 @@
+# enershift_backend/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.account.urls')),
     path('dashboard/', include('dashboard.urls')),
-    path('', lambda request: redirect('account_login'), name='home'),
+    path('', TemplateView.as_view(template_name='public/index.html'), name='home'),  # Real homepage
 ]
