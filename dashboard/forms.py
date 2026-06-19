3d3d99412bd986af49dcfdea6d784f6bd8c204b2
willbrad0803 <140106827+willbrad0803@users.noreply.github.com>
2026-06-19 22:03:04 +0100

    Update forms.py

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,7 +1,8 @@
+# dashboard/forms.py (update for constraints)
 from django import forms
 from .models import Site
 
 class SiteForm(forms.ModelForm):
     class Meta:
         model = Site
-        fields = ['name', 'postcode', 'industry_type', 'address', 'flexible_loads']
+        fields = ['name', 'postcode', 'industry_type', 'address', 'flexible_loads', 'temp_min_c', 'temp_max_c', 'shift_notice_hours', 'max_shift_duration_hours']
