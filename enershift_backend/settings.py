75d49b9bd2d4ecda116b366cf22cc8dd0abcfcf3
willbrad0803 <140106827+willbrad0803@users.noreply.github.com>
2026-07-12 12:17:43 +0100

    Update settings.py

--- enershift_backend/settings.py
+++ enershift_backend/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 SITE_ID = 1
 
